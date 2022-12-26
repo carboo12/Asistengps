a383031dd3d61f560f8715ed5c919962f4b907ac
carboo12 <carboo12@gmail.com>
2022-12-26 12:41:20 -0600

    ultimo cambio

--- src/main/java/com/example/gpsAsist/security/modelo/UsuarioPrincipal.java
+++ src/main/java/com/example/gpsAsist/security/modelo/UsuarioPrincipal.java
@@ -2,7 +2,6 @@ package com.example.gpsAsist.security.modelo;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -11,13 +10,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @SuppressWarnings("serial")
 public class UsuarioPrincipal implements UserDetails {
-	private String nombreUser;   
-    private String contraseña;  
-    private String nombreCompleto;
-    private Collection<? extends  GrantedAuthority> Authorities;
-    
-    
-    
+	private String nombreUser;
+	private String contraseña;
+	private String nombreCompleto;
+	private Collection<? extends GrantedAuthority> Authorities;
+
 	public UsuarioPrincipal(String nombreUser, String contraseña, String nombreCompleto,
 			Collection<? extends GrantedAuthority> authorities) {
 		super();
@@ -26,57 +23,63 @@ public class UsuarioPrincipal implements UserDetails {
 		this.nombreCompleto = nombreCompleto;
 		Authorities = authorities;
 	}
-	
-	public static UsuarioPrincipal build (Usuario usuario) {
+
+	public static UsuarioPrincipal build(Usuario usuario) {
 		List<GrantedAuthority> authorities = usuario.getTipousuarioList().stream()
 				.map(tipousuario -> new SimpleGrantedAuthority(tipousuario.getTipo())).collect(Collectors.toList());
-		return new UsuarioPrincipal(usuario.getNombreUser(), usuario.getContraseña(), usuario.getNombreCompleto(), authorities);
-		
+		return new UsuarioPrincipal(usuario.getNombreUser(), usuario.getContraseña(), usuario.getNombreCompleto(),
+				authorities);
+
 	}
-	
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return Authorities;
 	}
+
 	@Override
 	public String getPassword() {
 		// TODO Auto-generated method stub
 		return contraseña;
 	}
+
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
 		return nombreUser;
 	}
+
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	public String getNombreCompleto() {
 		return nombreCompleto;
 	}
+
 	public void setNombreCompleto(String nombreCompleto) {
 		this.nombreCompleto = nombreCompleto;
 	}
-	
-  
-	
 
 }
